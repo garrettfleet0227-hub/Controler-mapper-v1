2a29814ad51509ae57d89f0902351effe88985b5
garrettfleet0227-hub <garrettfleet0227@gmail.com>
2026-09-10 02:50:36 -0400

    Add MainActivity and ControllerMapperApp composable

---
+++ app/src/main/app/src/main/java/com/garrett/controllerMapper/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.garrett.controllerMapper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ControllerMapperApp()
+        }
+    }
+}
+
+@Composable
+fun ControllerMapperApp() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Controller Mapper")
+
+            Button(
+                onClick = {
+                    // Controller detection will be added next.
+                }
+            ) {
+                Text("Detect Controller")
+            }
+        }
+    }
+}
